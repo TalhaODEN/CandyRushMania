db4c581cda982f79de8899ce79ef7cd3c58589b5
Talha Öden <133509406+TalhaODEN@users.noreply.github.com>
2024-12-27 17:17:33 +0300

    Level 2,titreme efekti eklendi
    Level 2 eklendi,
    Kilitli level butonuna tıklandığında titreme efekti eklendi.

--- Assets/Scripts/Menu/LevelSelectManager.cs
+++ Assets/Scripts/Menu/LevelSelectManager.cs
@@ -16,15 +16,21 @@ public class Level
 public class LevelSelectManager : MonoBehaviour
 {
     private Level selectedLevel;
+    private bool isShaking;
 
     [Header("Levels")]
     [SerializeField] private Level[] levels;
+    [SerializeField] private GameObject levelsPanel;
 
     [Header("Selected Level Text")]
     [SerializeField] private Text SelectedLevelText;
 
     [Header("Forward Button")]
     [SerializeField] private GameObject forwardButton;
+
+    [Header("Locked Level Sprite")]
+    [SerializeField] private Sprite lockedLevelSprite;
+
 
     private void Start()
     {
@@ -33,7 +39,7 @@ public class LevelSelectManager : MonoBehaviour
             Button levelButton = level.levelButton.GetComponent<Button>();
             levelButton.onClick.AddListener(() => SelectLevel(level));
         }
-
+        isShaking = false;
         forwardButton.GetComponent<Image>().raycastTarget = false;
     }
 
@@ -43,6 +49,17 @@ public class LevelSelectManager : MonoBehaviour
         if(selectedLevel.levelIndex <= 1)
         {
             Debug.Log("BU BÖLÜMLER EKLENMEDİ!!!");
+            SelectedLevelText.text = "";
+            forwardButton.GetComponent<Image>().raycastTarget = false;
+            return;
+        }
+        if(selectedLevel.levelButton.GetComponent<Image>().sprite.name == lockedLevelSprite.name)
+        {
+            if (!isShaking)
+            {
+                StartCoroutine(ShakeButton());
+            }
+            selectedLevel = null;
             SelectedLevelText.text = "";
             forwardButton.GetComponent<Image>().raycastTarget = false;
             return;
@@ -71,6 +88,31 @@ public class LevelSelectManager : MonoBehaviour
         SceneManager.LoadScene(selectedLevel.levelIndex);
     }
 
-    
+    private IEnumerator ShakeButton()
+    {
+        isShaking = true;  
+        GridLayoutGroup gridComponent = levelsPanel.GetComponent<GridLayoutGroup>();
+        gridComponent.enabled = false;
+        float shakeAmount = 10f;
+        float shakeDuration = 0.5f;
+        float elapsedTime = 0f;
+
+        RectTransform rectTransform = selectedLevel.levelButton.GetComponent<RectTransform>();
+        Vector2 originalPosition = rectTransform.anchoredPosition;
+
+        while (elapsedTime < shakeDuration)
+        {
+            rectTransform.anchoredPosition = new Vector2(
+                originalPosition.x + UnityEngine.Random.Range(-shakeAmount, shakeAmount), originalPosition.y);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        rectTransform.anchoredPosition = originalPosition;
+        gridComponent.enabled = true;
+        isShaking = false;  
+    }
+
 
 }
